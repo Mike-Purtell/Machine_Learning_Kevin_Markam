d4fdba7372de3899d2e839ec9c13821cc8f75991
Michael Purtell <mpurtell1024@gmail.com>
2026-09-19 10:05:56 -0700

    Finished chap 14

--- Exercises_by_Chapter/chap_14.py
+++ Exercises_by_Chapter/chap_14.py
@@ -30,25 +30,29 @@ def _():
 
     from sklearn.preprocessing import StandardScaler
 
-    # from sklearn.feature_selection import SelectPercentile, chi2
-
     from sklearn.pipeline import make_pipeline
     from sklearn.impute import SimpleImputer
     from sklearn.preprocessing import OneHotEncoder
     from sklearn.feature_extraction.text import CountVectorizer    
     from sklearn.compose import make_column_transformer
     from sklearn.linear_model import LogisticRegression
-    # from sklearn.model_selection import cross_val_score
-    # from sklearn.model_selection import GridSearchCV
-    # from sklearn.feature_selection import SelectFromModel
-    # from sklearn.ensemble import ExtraTreesClassifier
-    # from sklearn.feature_selection import RFE
+
+    from sklearn.model_selection import cross_val_score
+
+    from sklearn.preprocessing import MaxAbsScaler
+
+    from sklearn.model_selection import GridSearchCV
+
+
     return (
         CountVectorizer,
+        GridSearchCV,
         LogisticRegression,
+        MaxAbsScaler,
         OneHotEncoder,
         SimpleImputer,
         StandardScaler,
+        cross_val_score,
         make_column_transformer,
         make_pipeline,
         os,
@@ -72,7 +76,9 @@ def _(mo):
 
     **My takeaways:**
 
-    - TBD
+    - Standardization of numerical features is not always necessary or helpful.
+    - models that benefit most from standardization are those that consider the distance between features, such as K-Nearest Neighbors and Support Vector Machines.
+    - feature standardization does not benefit tree-based models such as random forests.
 
     #### 14.1 Standardizing numerical features
     Some Machine Learning models benefit from feature standardization. That’s because the objective function of those models assumes that all features are centered around zero and have a variance of the same order of magnitude. If that assumption is incorrect, a given feature might dominate the objective function and the model won’t be able to learn from all of the features, thus reducing its performance.
@@ -156,44 +162,218 @@ def _(
     pipe = make_pipeline(ct, logreg)
     pipe.fit(X, y.to_series())
     pipe.predict(X_new)
-    return
+    return X, imp, imp_ohe, logreg, vect, y
 
 
 @app.cell
-def _(StandardScaler):
+def _(
+    StandardScaler,
+    imp,
+    imp_ohe,
+    logreg,
+    make_column_transformer,
+    make_pipeline,
+    vect,
+):
     scaler = StandardScaler()
+    imp_scaler = make_pipeline(imp, scaler)
+
+    ct_imp_scaler = make_column_transformer(
+        (imp_ohe, ['Embarked', 'Sex']),
+        (vect, 'Name'),
+        (imp_scaler, ['Age', 'Fare', 'Parch']),
+    )
+
+    scaler_pipe = make_pipeline(ct_imp_scaler, logreg)
+    scaler_pipe
+
+    return (scaler_pipe,)
+
+
+@app.cell
+def _(X, cross_val_score, scaler_pipe, y):
+    cross_val_score(
+        scaler_pipe,
+        X,
+        y.to_series(),
+        cv=5,
+        scoring='accuracy'
+    ).mean()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    That might be surprising, because regularized linear models often benefit from feature standardization.
+    However, our particular logistic regression solver (liblinear) happens to be robust to unscaled data,
+    and thus there was no benefit in this case.
+    The takeaway here is that you shouldn’t always assume that standardization of numerical features is
+    necessary.
+
+    ####  14.2 Standardizing all features
+    The previous lesson standardized the numerical features only. Other approach is to
+    standardize all features after transformation, even if they were not originally numerical.
+
+    We will add standardization as the second step in the Pipeline, in between the
+    ColumnTransformer and the model. But our ColumnTransformer outputs a sparse matrix, and
+    StandardScaler would destroy the sparseness by centering the data, likely resulting in a memory
+    issue.
+
+    We will use an alternative scaler called MaxAbsScaler. We’ll import it from the
+    preprocessing module and create an instance called scaler. MaxAbsScaler divides each feature
+    by its maximum value, which scales each feature to the range -1 to 1. Zeros are never changed,
+    and thus sparsity is preserved.
+    """)
     return
 
 
 @app.cell
-def _():
+def _(
+    MaxAbsScaler,
+    imp,
+    imp_ohe,
+    logreg,
+    make_column_transformer,
+    make_pipeline,
+    vect,
+):
+    scaler_max_abs = MaxAbsScaler()
+    ct_max_abs = make_column_transformer(
+        (imp_ohe, ['Embarked', 'Sex']),
+        (vect, 'Name'),
+        (imp, ['Age', 'Fare']),
+        ('passthrough', ['Parch'])
+    )
+    scaler_max_abs_pipe = make_pipeline(ct_max_abs, scaler_max_abs, logreg)
+    scaler_max_abs_pipe
+    return (scaler_max_abs_pipe,)
+
+
+@app.cell
+def _(X, cross_val_score, scaler_max_abs_pipe, y):
+    cross_val_score(
+        scaler_max_abs_pipe,
+        X,
+        y.to_series(),
+        cv=5,
+        scoring='accuracy'
+    ).mean()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    The cross-validate accuracy is 0.811, exactly the same as our baseline accuracy.
+    This result is not surprising, because MaxAbsScaler has no effect on the columns output by
+    OneHotEncoder and only a tiny effect on the columns output by CountVectorizer. Our
+    approach is mostly just affecting the numerical columns.
+
+    This example doesn’t benefit from standardization, but there are many cases where it will
+    help. If you want to experiment with standardization, try out both of the
+    approaches used in this chapter and pick the one that works better.
+
+    #### 14.3: How to see what scaling was applied to each feature?
+    If you’re interested in seeing what scaling was applied to each feature, you can fit the Pipeline and
+    then examine the scale_ attribute of the maxabsscaler step.
+
+    For example, the last three entries in the array correspond to the scaling for Age, Fare, and Parch.
+    These are simply the maximum values of Age, Fare, and Parch in X. As a reminder, this is the scaling
+    that will be applied to the features in X_new when making predictions.
+    """)
     return
 
 
 @app.cell
-def _():
+def _(X, scaler_max_abs_pipe, y):
+    scaler_max_abs_pipe.fit(X, y.to_series())
+    scaler_max_abs_pipe.named_steps['maxabsscaler'].scale_
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    As you might expect, the scale_ attribute is also available when using StandardScaler.
+
+    #### 14.4: How to I turn off feature standardization within a grid search?
+    Although grid search is usually just used to tune parameter values, you can actually use grid search
+    to turn on and off particular Pipeline steps. Thus you could use a grid search to decide whether or
+    not feature standardization should be included.
+    To demonstrate this, let’s create a small dictionary called scaler_params:
+    - The first entry tunes the C parameter of logistic regression. The dictionary key is the step name,
+    then two underscores, then the parameter name. The dictionary values are the possible values
+    for that parameter.
+    - The second entry is different: Rather than tuning the parameter of a Pipeline step, we’re
+    tuning the Pipeline step itself. In this case, the dictionary key is simply the step name assigned
+    by make_pipeline. The possible values are 'passthrough', which means skip this Pipeline
+    step, or a MaxAbsScaler instance, which means keep MaxAbsScaler in the Pipeline.
+
+    We’ll create scaler_grid using the scaler_pipe and scaler_params objects, and then run the grid
+    search as normal.
+    """)
     return
 
 
 @app.cell
-def _():
+def _(GridSearchCV, MaxAbsScaler, X, scaler_max_abs_pipe, y):
+    scaler_params = {}
+    scaler_params['logisticregression__C'] = [0.1, 1, 10]
+    scaler_params['maxabsscaler'] = ['passthrough', MaxAbsScaler()]
+    scaler_grid = GridSearchCV(
+        scaler_max_abs_pipe, 
+        scaler_params, 
+        cv=5, 
+        scoring='accuracy',
+        n_jobs=-1
+    )
+    scaler_grid.fit(X, y.to_series())
+    #scaler_grid.best_params_
+    return (scaler_grid,)
+
+
+@app.cell
+def _(pl, scaler_grid):
+    results_1 = pl.DataFrame(
+        {
+            "C": scaler_grid.cv_results_["param_logisticregression__C"].tolist(),
+            "param_maxabsscaler": [
+                "passthrough"
+                if value == "passthrough"
+                else type(value).__name__
+                for value in scaler_grid.cv_results_[
+                    "param_maxabsscaler"
+                ].tolist()
+            ],
+            "mean_test_score": scaler_grid.cv_results_["mean_test_score"].tolist(),
+            "rank_test_score": scaler_grid.cv_results_["rank_test_score"].tolist(),
+        },
+        strict=False,
+    )
+
+    results_1
     return
 
 
 @app.cell
-def _():
+def _(scaler_grid):
+    scaler_grid.best_params_
     return
 
 
-@app.cell
-def _():
-    params = {
-        'columntransformer__countvectorizer__ngram_range': [(1, 1), (1, 2)],
-        'columntransformer__pipeline__onehotencoder__drop': [None, 'first'],
-        'columntransformer__simpleimputer__add_indicator': [True, False],
-        'logisticregression__l1_ratio': [0.0, 1.0],  # 0.0 is L2 (Ridge), 1.0 is L1 (Lasso)
-        'logisticregression__C': [0.1, 1, 10] 
-    }
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    #### 14.5: Which models benefit from standardization?
+    Feature standardization is most useful for models that considers the distance between
+    features, such as K-Nearest Neighbors and Support Vector Machines.
+
+    It also tends to be useful with any models that incorporate regularization, such as linear or logistic
+    regression with an L1 or L2 penalty, though we saw earlier in the chapter that this doesn’t apply to all solvers.
+
+    Notably, feature standardization will not benefit any tree-based models such as random forests.
+    """)
     return
 
 
